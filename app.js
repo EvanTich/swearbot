5e535bc6f754272eaca18438cbdb079a8351fb36
Evan Tichenor <evan.tichenor@gmail.com>
2020-04-02 01:27:00 -0400

    Lots of changes and documentation for the masses
    Changed the way arguments are passed.
    Changed oddly named 'thing' to 'stat'.
    Changed 'stuff.js' file to 'config.js' because it makes more sense this way.
    Changed list command for future proofing.
    Moved help message to config.js.
    Removed useless `args[1] != 'swears'` check from top command.

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const NOTICE = '_**Notice: This bot, and programmer, does not condone using any of these words.**_\n\n';
 const DEBUG = false;
 
-const stuff = require('./stuff');
+const config = require('./config');
 const token = require('./token');
 
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
 
-var stats = require(stuff.file); // ez
 
-// var stats = {
-	// swears: {}, // 'swear key': # of times used
-	// users: {}, // 'user id': {'names': [], 'swears': {}, 'total': 0}
-	// guilds: {} // 'guild id': {'names': [], 'swears': {}, 'total': 0}
-// };
+/**
+ * Nice documentation for people who want it. I did it mostly for practice, but it is also useful.
+ */
+/**
+ * 
+ * @typedef {Object.<string, number>} NumDict Number dictionary with string keys.
+ * 
+ * @typedef {Object} Stat
+ * @property {String[]} names  The names this user/guild has used or is using.
+ * @property {NumDict}  swears An index of numbers keyed by a swear.
+ * @property {Number}   total  The total number of swears used.
+ * 
+ * @typedef {Object.<string, Stat>} StatDict Stat dictionary with string keys.
+ * 
+ * @typedef {Object} Stats
+ * @property {NumDict}  swears An index of numbers keyed by a swear. Used to keep count of individual swears.
+ * @property {StatDict} users  An index of Stats keyed by a user id string.
+ * @property {StatDict} guilds An index of Stats keyed by a guild id string.
+ */
+var stats = require(config.file); // ez (but bad)
 
+/**
+ * Formats the top n number of swears in the given swear dictionary into an easy to read message.
+ * @param {NumDict} swears  A swear stat dictionary.
+ * @param {Number}  [n = 5] The number of swears to list.
+ * @return {String} A formatted code block for Discord with the top swears of the swears object.
+ */
 function getSwearStats(swears, n = 5) {
 	let arr = Object.keys(swears)
 		.sort((a, b) => swears[b] - swears[a]);
@@ -26,15 +46,34 @@ function getSwearStats(swears, n = 5) {
 	return rtn + '```';
 }
 
-function getStats(thing) {
-	return `**${thing.names.join(', ')}**: Total: ${thing.total}\n${getSwearStats(thing.swears)}`;
+/**
+ * Formats the given stat object into an easy to read message.
+ * 
+ * 	`**${Name}**: Total: ${N}
+ *  ${getSwearStats}`
+ * 
+ * @see getSwearStats
+ * @param {Stat} stat A single user/guild stat object.
+ * @return {String} A string containing a formatted message with user/guild swear stats.
+ */
+function getStats(stat) {
+	return `**${stat.names.join(', ')}**: Total: ${stat.total}\n${getSwearStats(stat.swears)}`;
 }
 
-function getWorstStat(stat, statString, func = getStats, n = 5) {
+/**
+ * Formats the given stat object into a string containing a formatted message with the top n most vulgar users/guilds.
+ * @see getStats
+ * @param {StatDict} stat              A dictionary of Stat objects. Key is the id of a user/guild.
+ * @param {String}   statString        The string showing 
+ * @param {Number}   [n = 5]           The number of users/guilds to list.
+ * @param {Function} [func = getStats] The format function for the stat objects.
+ * @return {String} A string containing a formatted message with the top n most vulgar users/guilds.
+ */
+function getWorstStat(stat, statString, n = 5, func = getStats) {
 	let arr = Object.values(stat)
 		.sort((a, b) => b.total - a.total);
 	let rtn = `Top ${n} **Worst** ${statString}: \n`;
-	// add top n worst things 
+	// add top n worst users/guilds
 	for(let i = 0; i < n && i < arr.length; i++) {
 		rtn += func(arr[i]);
 	}
@@ -42,51 +81,91 @@ function getWorstStat(stat, statString, func = getStats, n = 5) {
 	return rtn;
 }
 
+/**
+ * Formats the given guild object into a string with it's swear stats.
+ * @see discord.js
+ * @see getStats
+ * @param {Guild} guild The guild to get stats for.
+ * @return {String} A string with the given guild stats.
+ */
 function getGuildStats(guild) {
 	if(guild.id in stats.guilds)
 		return getStats(stats.guilds[guild.id]);
 	return `Nobody in ${guild.name} has said any bad words.`;
 }
 
-function getWorstGuilds(n = 5) {
-	return getWorstStat(stats.guilds, 'Guilds');
-}
-
+/**
+ * Formats the given guild object into a string with it's swear stats.
+ * @see discord.js
+ * @see getStats
+ * @param {User} user The user to get stats for.
+ * @return {String} A string with the given user stats.
+ */
 function getUserStats(user) {
 	if(user.id in stats.users)
 		return getStats(stats.users[user.id]);
 	return `${user.username} has not uttered *any* profanities while under the watch of the swear bot.`;
 }
 
-function getWorstUsers(n = 5) {
-	return getWorstStat(stats.users, 'Users');
+/**
+ * Returns a string containing a formatted message with the top n most vulgar guilds.
+ * @see getWorstStat
+ * @param {Number} [n = 5] The number of guilds to list.
+ * @return {String} A string containing a formatted message with the top n most vulgar guilds.
+ */
+function getWorstGuilds(n = 5) {
+	return getWorstStat(stats.guilds, 'Guilds', n);
 }
 
-function doStat(thing, name, swear) {
-	if(thing == null) {
-		thing = {
+/**
+ * Returns a string containing a formatted message with the top n most vulgar users.
+ * @see getWorstStat
+ * @param {Number} [n = 5] The number of users to list.
+ * @return {String} A string containing a formatted message with the top n most vulgar users.
+ */
+function getWorstUsers(n = 5) {
+	return getWorstStat(stats.users, 'Users', n);
+}
+
+/**
+ * Increments the given swear for the stat object.
+ * @param {Stat}   stat  The swear stat object to change.
+ * @param {String} name  The name of the user/guild.
+ * @param {String} swear The swear to increment.
+ * @return {Stat} The swear stat object.
+ */
+function doStat(stat, name, swear) {
+	if(stat == null) {
+		stat = {
 			'names': [name],
 			'swears': {},
 			'total': 0
 		};
 	}
-	if(!thing.names.includes(name)) {
-		thing.names.push(name);
+	if(!stat.names.includes(name)) {
+		stat.names.push(name);
 	}
 
-	if(thing.swears[swear] == null)
-		thing.swears[swear] = 0; // define as initial value if not
+	if(stat.swears[swear] == null)
+		stat.swears[swear] = 0; // define as initial value if not
 
-	thing.swears[swear]++;
-	thing.total++;
+	stat.swears[swear]++;
+	stat.total++;
 
-	return thing;
+	return stat;
 }
 
+/**
+ * Controls all of the bot's commands. It does what it's expected to do. Hopefully.
+ * @param {Message}  msg  The original discord.js message. Contains all necessary data for any bot.
+ * @param {String[]} args The arguments of the command, excluding the bot prefix.
+ * @return {(String | boolean | RichEmbed)} The message that is expected given the arguments. 
+ * 		Returns false if there is no message. Can also return a RichEmbed for inviting.
+ */
 function commands(msg, args) {
-	switch(args[1]) {
+	switch(args[0].toLowerCase()) {
 		// case 'about': // now that I think about it I don't like random people contacting me through discord
-		// 	return `Created by <@${stuff.devId}> because I thought it was an ok concept.`;
+		// 	return `Created by <@${config.devId}> because I thought it was an ok concept.`;
 		case 'invite':
 		case 'link':
 			return new Discord.RichEmbed()
@@ -97,45 +176,37 @@ function commands(msg, args) {
 				.setColor('GREEN')
 				.setThumbnail(client.user.avatarURL);
 		case 'feedback':
-			let feedback = msg.content.substring(16).trim();
+			let feedback = args.slice(1).join(' ');
 			if(!feedback) return false; // silent fail
 
 			let data = `${msg.author.tag}: ${feedback}\n`; // '!swear feedback '.length == 16
-			log(stuff.feedbackFile, data);
+			log(data, config.feedbackFile);
 
 			return 'Thanks for the feedback!';
 		case 'help':
-			// this spacing is for my own sanity
-			return 'Help: ```!swear [...]\n' + 
-			'  help: shows this help message.\n' + 
-			'  list: shows the list of words that are counted.\n' + 
-			'  top [users, servers, swears]: shows the global statistics of the bot.\n' + 
-			'  here | stats: shows the statistics for the current server.\n' + 
-			'  feedback [message...]: gives feedback to the developer of the bot. Thanks!\n' + 
-			'  invite | link: gives a link for adding the bot to your own server.\n' + 
-			'  @mention(s): gives user statistics based on who you mention in the message.```';
+			return config.help;
 		case 'list':
-			return `${NOTICE}*Swears that are counted:*\n${stuff.swears.join(', ')}.`;
+			return `I am not at liberty to say the whole list, but I keep track of ${config.swears.length} swears.`;
 		case 'top':
-			if(args.length >= 3 && args[2] != 'swears') {
-				if(args[2] == 'users') {
-					return `${NOTICE}${getWorstUsers()}`;
-				} else if(args[2] == 'servers' || args[2] == 'guilds') {
-					return `${NOTICE}${getWorstGuilds()}`;
-				} 
+			if(args.length >= 2) {
+				switch(args[1].toLowerCase()) {
+					case 'users': return `${NOTICE}${getWorstUsers()}`;
+					case 'servers':
+					case 'guilds': return `${NOTICE}${getWorstGuilds()}`;
+				}
 				// TODO? maybe send a helpful message here
 			}
 			return `${NOTICE}Top 5 Swears:\n${getSwearStats(stats.swears)}\nGrand Total: ${stats.total}`;
 		case 'shutdown': // dev only
 		case 'restart':
-			if(msg.author.id == stuff.devId) {
+			if(msg.author.id == config.devId) {
 				// special one
 				msg.author.send('Shutting Down...').then(() => client.destroy()).then(() => process.exit(0));
 			}
 			return false; // just in case
 		case 'here':
 		case 'stats':
-			return `${NOTICE}**Current Server Statistics:**\n${getGuildStats(msg.guild.id)}`;
+			return `${NOTICE}**Current Guild Statistics:**\n${getGuildStats(msg.guild)}`;
 		default:
 			if(msg.mentions.users.size == 0)
 				return 'No users mentioned. Please @mention the user(s) you want to see statistics for.';
@@ -150,7 +221,13 @@ function commands(msg, args) {
 	}
 }
 
-function log(file, data, debugStr = data) {
+/**
+ * Appends log data to a file. Basically unused.
+ * @param {String} data The data to be appended to the log.
+ * @param {String} file Path to the log file.
+ * @param {String} debugStr String to print if in debug mode.
+ */
+function log(data, file = config.logFile, debugStr = data) {
 	fs.appendFile(file, data, err => {
 		if(err) console.error(err);
 		if(DEBUG) console.log(debugStr);
@@ -158,7 +235,7 @@ function log(file, data, debugStr = data) {
 }
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
@@ -166,19 +243,22 @@ client.on('message', msg => {
 		return; // ignore swearbot messages
 
 	let lower = msg.content.toLowerCase();
-	if(lower.startsWith('!swear')) {
-		let args = lower.split(' ');
-		let send = commands(msg, args);
-		if(send) {
-			msg.channel.send(send).catch(console.error);
+	for(let prefix of config.prefixes) {
+		if(lower.startsWith(prefix)) {
+			let args = msg.content.slice(prefix.length).trim().split(/ +/g);
+			let send = commands(msg, args);
+			if(send) {
+				msg.channel.send(send).catch(console.error);
+			}
+			return; // skip the rest because we just got a command
 		}
-		return; // skip the rest because we just got a command
 	}
+	
 
 	let author = msg.author;
 	let mGuild = msg.guild;
 	
-	for(let swear of stuff.swears) {
+	for(let swear of config.swears) {
 		if(lower.includes(swear)) { // if the message includes the swear
 			if(stats.swears[swear] == null)
 				stats.swears[swear] = 0; // define as initial value if not
@@ -188,7 +268,7 @@ client.on('message', msg => {
 			if(mGuild.available) {
 				stats.guilds[mGuild.id] = doStat(stats.guilds[mGuild.id], mGuild.name, swear);
 			} else {
-				log(stuff.logFile, `Server outage detected: ${new Date().toISOString()}`);
+				log(`Server outage detected: ${new Date().toISOString()}`);
 			}
 			
 			stats.users[author.id] = doStat(stats.users[author.id], author.username, swear);
@@ -201,7 +281,7 @@ client.login(token.token).then(() => client.user.setActivity('!swear help'));
 setInterval( () => {
 	// periodically save stats to file
 	let data = JSON.stringify(stats, null, 2); // make human readable
-	fs.writeFile(stuff.file, data, err => {
+	fs.writeFile(config.file, data, err => {
 		if(err) console.error(err);
 		if(DEBUG) console.log('Data written to file, unless printed otherwise.');
 	});
@@ -212,8 +292,8 @@ setInterval( () => {
 		client.user.setActivity('!swear help');
 	} else {
 		// 50% chance for other fun things
-		let i = Math.floor(Math.random() * stuff.activities.length);
+		let i = Math.floor(Math.random() * config.activities.length);
 		let type = ['PLAYING', 'LISTENING', 'WATCHING'][Math.floor(i / 4)];
-		client.user.setActivity(stuff.activities[i], { type });
+		client.user.setActivity(config.activities[i], { type });
 	}
 }, 300000); // every 5 minutes
---
+++ config.js
@@ -0,0 +1,47 @@
+// simple js file for easy access to some constants
+const devId        = '203281664699269121';
+const file         = './stats.json';
+const logFile      = './app.log';
+const feedbackFile = './feedback.txt';
+const prefixes     = ['!swear', 's.'];
+
+const help = `Help: \`\`\`!swear [...]
+  help: Shows this help message.
+  list: Shows the list of words that are counted.
+  top [users | servers | swears] [n = 5]: shows the top n global statistics of the bot.
+  here | stats: shows the statistics for the current server.
+  feedback <message...>: gives feedback to the developer of the bot. Thanks!
+  invite | link: gives a link for adding the bot to your own server.
+  @mention(s): gives user statistics based on who you mention in the message.\`\`\``;
+
+// fun bits and bobs
+const activities = [
+	'with fire', 'with naughty words', 'in the sand', 'Dark Souls', // playing
+	'everyone', 'more bad words', 'Mozart', 'some screaming', // listening to
+	'the chat', 'someone yell', 'interesting videos', 'DEFCON 1' // watching
+];
+
+// this is probably the most i've used any swear word in a single hour
+const swears = [
+	'fuck', 'cunt', 'ass', 'damn', 'damnit',
+	'shit', 'goddamn', 'asshole', 'dyke', 'fuckwad',
+	'turk', 'asshat', 'jackass', 'fucker', 'furry',
+	'motherfucker', 'frigger', 'godsdamn', 'hell',
+	'arse', 'bastard', 'bitch', 'bollocks', 'holy shit',
+	'horseshit', 'jesus fuck', 'prick', 'shitass', 'shit ass',
+	'slut', 'son of a bitch', 'twat', 'nigger', 'fag',
+	// nice words
+	'frick', 'dang', 'darn', 'hecc', 'hekk', 'heck', 'poo', 'pee',
+	'dingus'
+];
+
+module.exports = {
+	devId,
+	file,
+	logFile,
+	feedbackFile,
+	prefixes,
+	help,
+	activities,
+	swears
+};
